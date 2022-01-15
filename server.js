3643ab71248fc0e2b9c27ee237b1ae1df8f70d8b
RK03ind <gamitech95@gmail.com>
2022-01-15 23:39:50 +0530

    express middlewares added

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const http = require("http");
 const express = require("express");
 const app = express();
-
 app.listen(3000);
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-users", (req, res) => {
+  res.send(
+    `<form method="POST" action="/log-data"><input type="text" name="user"><button type="submit">Submit</button></form>`
+  );
+});
+
+app.use("/log-data", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res) => {
+  res.send("<h1>its in root route </h1>");
+});
